5f5f0691230140097d151a0fd95554a3c8c05bac
Minhyeok Park <pmh_only@pmh.codes>
2023-02-27 18:39:27 +0900

    fix: call async while handling a request

--- functions/addAddress.ts
+++ functions/addAddress.ts
@@ -5,14 +5,14 @@ interface RequestBody {
   idToken: string
 }
 
-const publicKey = fetch('https://center.gbsw.hs.kr/publickey.pub').then(
-  async (res) => await res.text()
-)
-
 export const onRequestPost: PagesFunction = async ({ request }) => {
   const { email, idToken } = await request.json<RequestBody>()
 
-  const ecPublicKey = await importSPKI(await publicKey, 'ES256')
+  const pubkey = await fetch('https://center.gbsw.hs.kr/publickey.pub').then(
+    async (res) => await res.text()
+  )
+
+  const ecPublicKey = await importSPKI(pubkey, 'ES256')
   const { payload } = await jwtDecrypt(idToken, ecPublicKey, {
     issuer: 'https://center.gbsw.hs.kr',
     audience: process.env.VITE_CLIENT_ID
