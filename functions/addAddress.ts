a9ce65584d1469f083159718612f6e4db6e7a118
Minhyeok Park <pmh_only@pmh.codes>
2023-02-27 17:31:45 +0900

    feat: add jwt verification

--- functions/addAddress.ts
+++ functions/addAddress.ts
@@ -1,12 +1,24 @@
+import { verify } from 'jsonwebtoken'
+
 interface RequestBody {
   email: string
   idToken: string
 }
 
+const publicKey = fetch('https://center.gbsw.hs.kr/publickey.pub').then(
+  async (res) => await res.text()
+)
+
 export const onRequestPost: PagesFunction = async ({ request }) => {
   const { email, idToken } = await request.json<RequestBody>()
 
-  console.log(email, idToken)
+  const { data } = verify(idToken, await publicKey, {
+    algorithms: ['ES256'],
+    audience: process.env.VITE_CLIENT_ID,
+    issuer: 'https://center.gbsw.hs.kr'
+  }) as any
+
+  console.log(data.login, email)
 
   return Response.json({
     ok: true
